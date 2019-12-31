32f6d27913c2b7594d6c05821a610db0c8651080
Sameer Mahant <mahant.sameer@gmail.com>
2019-12-31 20:06:59 +0530

    Added script to install/update Visual Studio Code IDE

---
+++ VSCode_install_update.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -eu
+
+# I prefer to use softwares in portable mode, as its easy to remove without disturbing the system stability.
+# This script installs / updates the Visual Studio Code IDE in the /opt directory
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run with root privileges."
+   exit 1
+fi
+
+VISUAL_STUDIO_CODE_URL="https://update.code.visualstudio.com/latest/linux-x64/stable"
+TEMPORARY_OUTPUT_FILE_LOCATION="/tmp/vscode.tar.gz"
+
+echo "Downloading VSCode to /tmp/"
+wget -q "${VISUAL_STUDIO_CODE_URL}" -O "${TEMPORARY_OUTPUT_FILE_LOCATION}"
+
+if [ -f "${TEMPORARY_OUTPUT_FILE_LOCATION}" ]; then
+    echo "Download completed."
+else
+    echo "Download failed."
+    exit
+fi
+
+echo "Extracting to /tmp/ ..."
+cd /tmp/
+tar -xf vscode.tar.gz
+echo "Completed extracting."
+# Note: Extracted folder name is 'VSCode-linux-x64'
+
+echo "Removing old version ..."
+rm -rf /opt/VSCode-linux-x64 || true
+
+echo "Setting up new version ..."
+mv VSCode-linux-x64 /opt/
+
+echo "Done."
